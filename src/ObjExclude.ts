fa8556273769a16e7536593bc155e01c94af56e7
pinyin <bob432091@gmail.com>
2018-06-02 11:30:55 +0800

    0.20 ensure that ObjectExclude returns object

--- src/ObjExclude.ts
+++ src/ObjExclude.ts
@@ -1,6 +1,7 @@
 export type ObjExclude<A extends object,
     B extends object,
-    K extends Exclude<keyof A, keyof B> = Exclude<keyof A, keyof B>> = {
-    [Key in K]: A[K]
-}
+    K extends Exclude<keyof A, keyof B> = Exclude<keyof A, keyof B>> =
+    { [Key in K]: A[K] } extends never ?
+        {} :
+        { [Key in K]: A[K] }
 
