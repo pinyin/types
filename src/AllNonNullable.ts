bf31bf36c6cd0ec0d44af076ae476dab1cc53c75
pinyin <bob432091@gmail.com>
2018-06-07 18:41:06 +0800

    0.0.23 fix AllNonNullable

--- src/AllNonNullable.ts
+++ src/AllNonNullable.ts
@@ -1,3 +1,13 @@
-export type AllNonNullable<T, K extends keyof T = keyof T> = Required<{
-    [Key in K]: NonNullable<T[Key]>
-}>
+export type AllNonNullable<T, K extends keyof T = keyof T> = {
+    [key in K]-?: AllNonNullable<NonNullable<T[key]>>
+}
+//
+// const a: AllNonNullable<{
+//     b?: {
+//         c?: {
+//             d: 'd'
+//         }
+//     }
+// }> = {} as any
+//
+// const d = a.b.c.d
--- src/ObjExclude.ts
+++ src/ObjExclude.ts
@@ -1,4 +1,4 @@
 export type ObjExclude<A extends object, B extends object, KB extends keyof B = keyof B> = A & {
-    [key in KB]?: never
+    [key in KB]?: never // TODO key still exists
 }
 
