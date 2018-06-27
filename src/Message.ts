db6d4e907229165573cd89118beba806c8be0048
pinyin <bob432091@gmail.com>
2018-06-27 12:37:41 +0800

    support partial shape in Message

--- src/Message.ts
+++ src/Message.ts
@@ -2,10 +2,33 @@ import {IsNothing} from './IsNothing'
 import {nothing} from './nothing'
 import {TagKey} from './TagKey'
 
-export type Message<S extends object, K extends keyof S = keyof S> = {
+export type Message<S extends object, K extends Exclude<keyof S, nothing> = Exclude<keyof S, nothing>> = {
     [key in K]: IsNothing<S[key]> extends true ?
         { [TagKey]: key, payload?: nothing } :
         { [TagKey]: key, payload: S[key] }
 }[K]
 
 export type PayloadOf<M extends Message<any>> = M['payload']
+
+// type S = {
+//     a: number
+//     b?: string
+//     c: undefined
+// }
+//
+// type M = Message<S>
+//
+// const m = {} as M
+//
+// if(m.type === 'a') {
+//     m.payload // number
+// }
+//
+// if(m.type === 'b') {
+//     m.payload // string | undefined // TODO test types
+// }
+//
+// if(m.type === 'c') {
+//     m.payload // nothing
+//     let b: M = {type: 'c'}
+// }
