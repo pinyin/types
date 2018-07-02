c6400ab63bca29db9ad7c274baff9a2086e861f2
pinyin <bob432091@gmail.com>
2018-07-02 10:24:11 +0800

    update TagOf

--- src/TagOf.ts
+++ src/TagOf.ts
@@ -1,10 +1,4 @@
-import {Tag} from './Tag'
-import {Tagged} from './Tagged'
-import {TagKey} from './TagKey'
 import {TagMap} from './TagMap'
 
-export type TagOf<D extends Tagged | TagMap> =
-    D extends Tagged ?
-        D[TagKey] :
-        keyof D & Tag
+export type TagOf<D extends TagMap> = NonNullable<keyof D>
 
