d467dfa65b548c16c09a220199ac2d0eb761fb58
pinyin <bob432091@gmail.com>
2018-07-09 22:05:01 +0800

    use TagMap

--- src/tagged union/TaggedByMap.ts
+++ src/tagged union/TaggedByMap.ts
@@ -1,29 +1,7 @@
 import {TagFromMap} from './TagFromMap'
 import {Tagged} from './Tagged'
+import {TagMap} from './TagMap'
 
-export type TaggedByMap<S extends object> = {
+export type TaggedByMap<S extends TagMap> = {
     [key in TagFromMap<S>]: Tagged<key, S[key]>
 }[TagFromMap<S>]
-
-// type S = {
-//     a: number
-//     b?: string
-//     c: undefined
-// }
-//
-// type M = Message<S>
-//
-// const m = {} as M
-//
-// if(m.type === 'a') {
-//     m.payload // number
-// }
-//
-// if(m.type === 'b') {
-//     m.payload // string | undefined // TODO test types
-// }
-//
-// if(m.type === 'c') {
-//     m.payload // nothing
-//     let b: M = {type: 'c'}
-// }
